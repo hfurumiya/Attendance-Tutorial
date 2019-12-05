7582d47df9d508e02a19594a072f988589687909
hfurumiya <bosnyctokosa@gmail.com>
2019-12-05 07:19:27 +0000

    Finish editing-users ８章終わった

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,8 @@ module SessionsHelper
   end
   
   # 永続的セッションを記憶します（Userモデルを参照）
-  def remembrer(user)
-    user.remembrer
+  def remember(user)
+    user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
@@ -29,18 +29,34 @@ module SessionsHelper
   # それ以外の場合はcookiesに対応するユーザーを返します
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remembrer_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
   
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します
+  def current_user?(user)
+    user == current_user
+  end
+  
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します
   def logged_in?
     !current_user.nil?
+  end
+  
+  # 記憶しているURL(またはデフォルトURL)にリダイレクトします
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶します
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
